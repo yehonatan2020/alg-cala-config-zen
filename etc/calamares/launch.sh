88675d627416c06fa5bb157b6584ac989758ecb5
yehonatan2020 <yehonatan@yandex.ru>
2022-11-16 11:56:49 +0000

    zen compatibility

--- etc/calamares/launch.sh
+++ etc/calamares/launch.sh
@@ -5,7 +5,7 @@ KERNEL=`uname -r`
 
 if [[ -d "/run/archiso/copytoram" ]]; then
 	sudo sed -i -e 's|/run/archiso/bootmnt/arch/x86_64/airootfs.sfs|/run/archiso/copytoram/airootfs.sfs|g' "$DIR"/modules/unpackfs.conf
-	sudo sed -i -e "s|/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz-linux-mainline|/usr/lib/modules/$KERNEL/vmlinuz|g" "$DIR"/modules/unpackfs.conf
+	sudo sed -i -e "s|/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz-linux-zen|/usr/lib/modules/$KERNEL/vmlinuz|g" "$DIR"/modules/unpackfs.conf
 fi
 
 sudo -E calamares -d
